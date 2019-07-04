01db97d70661669da94399ea1cff75f0c04ee89d
wujl12 <wujl12@lenovo.com>
2019-07-04 17:44:13 +0800

    添加Thread

---
+++ J8Lock/src/main/java/thread/ThreadMain.java
@@ -0,0 +1,10 @@
+package thread;
+
+/**
+ * 实现一个线程池
+ */
+public class ThreadMain {
+    public static void main(String[] args){
+
+    }
+}
---
+++ J8Lock/src/main/java/thread/inface/Executor.java
@@ -0,0 +1,5 @@
+package thread.inface;
+
+public interface Executor {
+
+}
