e1689195aaa8e63229713ca141ee7f45e0f7f19b
wujl12 <wujl12@lenovo.com>
2019-07-05 17:42:40 +0800

    实现SingLock的ShareObject一部分

--- J8Lock/src/main/java/lock/singlelock/ShareObject.java
+++ J8Lock/src/main/java/lock/singlelock/ShareObject.java
@@ -1,5 +1,16 @@
 package lock.singlelock;
 
+import lock.Lock;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShareObject {
+
+    private Lock singleLock = new SingleLock();
+
+    private List<Character> chars = new ArrayList<>();
+
     
+
 }
