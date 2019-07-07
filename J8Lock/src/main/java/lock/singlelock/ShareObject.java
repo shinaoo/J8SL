185df37ce3bbe407043615d085ccffd28c95cafa
125859320@qq.com <125859320@qq.com>
2019-07-07 23:43:42 +0800

    共享数据的操作 “

--- J8Lock/src/main/java/lock/singlelock/ShareObject.java
+++ J8Lock/src/main/java/lock/singlelock/ShareObject.java
@@ -10,7 +10,28 @@ public class ShareObject {
     private Lock singleLock = new SingleLock();
 
     private List<Character> chars = new ArrayList<>();
+    private int length;
 
-    
+    public ShareObject(int length) {
+        this.length = length;
+    }
+
+    public char[] read() throws InterruptedException {
+        try {
+            singleLock.lock();
+            char[] result = new char[chars.size()];
+            for (int i = 0;i < length;i++){
+                result[i] = chars.get(i);
+            }
+            return result;
+        }finally {
+            singleLock.unlock();
+        }
+
+    }
+
+
+
 
 }
+
