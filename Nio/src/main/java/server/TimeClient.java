4cff93454a7f9a3e98be29b9a90f86fb0ff89d75
wujl12 <wujl12@lenovo.com>
2019-07-19 13:42:05 +0800

    添加JavaNIO

---
+++ Nio/src/main/java/server/TimeClient.java
@@ -0,0 +1,9 @@
+package server;
+
+public class TimeClient {
+
+    public static void main(String[] args){
+        int port = 8080;
+        new Thread(new TimeClientHandle("127.0.0.1",port),"TimeClientHandle-1").start();
+    }
+}
