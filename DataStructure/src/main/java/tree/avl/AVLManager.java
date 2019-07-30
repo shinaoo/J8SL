0018503dd6b399b7854f63302144d431085f6ca7
125859320@qq.com <125859320@qq.com>
2019-07-30 23:19:31 +0800

    添加AVL树的一些说明

--- DataStructure/src/main/java/tree/avl/AVLManager.java
+++ DataStructure/src/main/java/tree/avl/AVLManager.java
@@ -15,4 +15,18 @@ public class AVLManager {
     public boolean deleteValue(int value){
         return false;
     }
+
+    private Node findMax(Node node){
+        while(node.right != null){
+            node = node.right;
+        }
+        return node;
+    }
+
+    private Node findMin(Node node){
+        while (node.left != null){
+            node = node.left;
+        }
+        return node;
+    }
 }
