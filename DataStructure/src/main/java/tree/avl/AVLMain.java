9e34a0f142db93937a6e90ab1a2f99068940af13
wujl12 <wujl12@lenovo.com>
2019-08-01 17:53:31 +0800

    AVL还有一些问题

--- DataStructure/src/main/java/tree/avl/AVLMain.java
+++ DataStructure/src/main/java/tree/avl/AVLMain.java
@@ -6,6 +6,14 @@ public class AVLMain {
     //带有平衡条件:每个节点的左右子树高度之差的绝对值最多为1
     //其实可以看出只是一个带有平衡功能的二叉查找树
     public static void main(String[] args){
+        Node root = new Node(10);
+        AVLManager manager = new AVLManager(root);
 
+        manager.addValue(20);
+        manager.addValue(30);
+        manager.addValue(40);
+        manager.addValue(50);
+        manager.addValue(25);
+        manager.printTreeByPreOrder();
     }
 }
--- DataStructure/src/main/java/tree/avl/AVLManager.java
+++ DataStructure/src/main/java/tree/avl/AVLManager.java
@@ -1,5 +1,7 @@
 package tree.avl;
 
+import org.omg.CORBA.NO_IMPLEMENT;
+
 public class AVLManager {
 
     private Node root;
@@ -14,23 +16,23 @@ public class AVLManager {
     }
 
     public boolean addNode(Node node) {
-        return (root = addNodeInternal(root,node)) != null;
+        return (root = addNodeInternal(root, node)) != null;
     }
 
 
-    private Node addNodeInternal(Node root,Node node){
-        if (root == null){
-            root = node;
+    private Node addNodeInternal(Node root, Node node) {
+        if (root == null) {
+            root = new Node(node.value);
             return root;
         }
         if (root.value < node.value)
-            root.left = addNodeInternal(root.left,node);
+            root.left = addNodeInternal(root.left, node);
         else if (root.value > node.value)
-            root.right = addNodeInternal(root.right,node);
+            root.right = addNodeInternal(root.right, node);
         else
             return root;
 
-        root.height = 1 + Math.max(height(root.left),height(root.right));
+        root.height = 1 + Math.max(height(root.left), height(root.right));
 
         int balance = getBalanceFactor(root);
         if (balance > 1 && node.value < root.left.value)
@@ -39,12 +41,12 @@ public class AVLManager {
         if (balance < -1 && node.value > root.right.value)
             return leftRotation(root);
 
-        if (balance > 1 && node.value > root.left.value){
+        if (balance > 1 && node.value > root.left.value) {
             root.left = leftRotation(root.left);
             return rightRotation(root);
         }
 
-        if (balance < -1 && node.value < root.right.value){
+        if (balance < -1 && node.value < root.right.value) {
             root.right = rightRotation(root.right);
             return leftRotation(root);
         }
@@ -53,29 +55,31 @@ public class AVLManager {
     }
 
 
-
     public boolean deleteValue(int value) {
         return false;
     }
 
     //左旋
     private Node leftRotation(Node node) {
-        Node tmp = node.right;
-        node.right = tmp.left;
-        tmp.left = node;
-        tmp.height = Math.max(height(tmp.left),height(tmp.right)) + 1;
-        node.height = Math.max(height(node.left),height(node.right)) + 1;
-        return tmp;
+        Node y = node.right;
+        Node T2 = y.right;
+        y.left = node;
+        node.right = T2;
+
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+        return y;
     }
 
     //右旋
     private Node rightRotation(Node node) {
-        Node tmp = node.left;
-        node.left = tmp.right;
-        tmp.right = node;
-        tmp.height = Math.max(height(tmp.left),height(tmp.right)) + 1;
-        node.height = Math.max(height(node.left),height(node.right)) + 1;
-        return tmp;
+        Node x = node.left;
+        Node T2 = x.right;
+        x.right = node;
+        node.left = T2;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+        return x;
     }
 
     //查找节点高度
@@ -87,16 +91,27 @@ public class AVLManager {
         return Math.max(leftHeight, rightHeight) + 1;
     }
 
-    private int height(Node node){
+    private int height(Node node) {
         if (node == null)
             return 0;
         return node.height;
     }
 
-    private int getBalanceFactor(Node node){
+    private int getBalanceFactor(Node node) {
         if (node == null)
             return 0;
         return height(node.left) - height(node.right);
+    }
+
+    public void printTreeByPreOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node root) {
+        if (root != null)
+            System.out.printf("%d--", root.value);
+        preOrder(root.left);
+        preOrder(root.right);
     }
 
 }
